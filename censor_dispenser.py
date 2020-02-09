c218bc5e8516d43e41bdc89f65712bb244be08f1
wjongeneel <wjongeneel@users.noreply.github.com>
2020-02-09 12:09:03 +0100

    Functie toegevoegd om woorden in teksten te vervangen met *-characters

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,3 +3,13 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+# replaces the string-variabe word in the string-variable text by *-characters
+def censor_word(word, text):
+    # determine the replacement_string by detemining the length of the string and replacing every character in the string by *-characters
+    replacement_string = ""
+    for i in range(0, len(word)):
+        replacement_string = replacement_string + "*"
+    # replaces the string-variable word in the string-variable text by string-variable replacement_string
+    new_text = text.replace(word, replacement_string)
+    return new_text
